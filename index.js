b2f5155d74158bab31f646f2dcef5dbd0fae99f5
manoj1202 <manoj.appalaneni@gmail.com>
2018-06-05 08:43:23 -0500

    sample Redux

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function createStore(){
     let state;
+    let listeners = [];
     
     const getState = () => state;
+    const subScribe = (listener) => {
+        listeners.push(listener);
+    }
 
     return{
         getState
