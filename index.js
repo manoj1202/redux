ded29bd099a870839c45cfd4cc82a5da2583d43a
manoj1202 <manoj.appalaneni@gmail.com>
2018-06-05 10:28:13 -0500

    sample Redux

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ function createStore(){
 function todo(state =[], action){
     switch(action.type){
         case "ADD_TODO":
-        return state.filter.concat([action.todo])
+        return state.concat([action.todo])
+        case "REMOVE_TODO":
+        return state.filter((todo) => todo.id !== action.id)
+        case "TOGGLE_TODO":
+        return state.map((todo) => todo.id !== action.id ? todo :
+            Object.assign({},todo,{complete: !todo.complete})
+        )
+        default :
+        return state
     }
 }
